e8a1e376b81a8a02bcef367e6b8a150b923dc6d2
Oskarlingm <oskarlingmoisesmnedezmercedes@gmail.com>
2025-07-10 06:52:24 +0300

    Modifique el main para solo tener dos objetos

--- Program.cs
+++ Program.cs
@@ -9,19 +9,18 @@ namespace Escuela
         {
             Estudiante estudiante1 = new Estudiante("Gary", 20221018, "Software", 100.0f);
             Estudiante estudiante2 = new Estudiante("Oskarr", 20089323, "Software", 100.00f);
-            Estudiante estudiante3 = new Estudiante("Ossas", 20245634, "Redes", 60.0f);
-            Estudiante estudiante4 = new Estudiante("Marcos", 2022342, "Mecatronica", 50.0f);
-            Estudiante estudiante5 = new Estudiante("Kaory", 2022503, "Multimedia", 100.0f);
+            
 
             estudiante1.MostrarInformacion();
-            estudiante3.MostrarInformacion();
+            estudiante2.MostrarInformacion();
 
 
-            System.Console.WriteLine($"Estudiante a pasado? {estudiante1.EsAprobado()}"); 
-            System.Console.WriteLine($"Estudiante a pasado? {estudiante3.EsAprobado()}");
+            System.Console.WriteLine($"Estudiante a pasado? {estudiante1.EsAprobado()}");
+            System.Console.WriteLine($"Estudiante a pasado? {estudiante2.EsAprobado()}");
 
 
         }
+        
 
 
 
